546784ac68361eae797ee9da5a98d5e2ec1fd6f2
commodore20 <43285184+commodore20@users.noreply.github.com>
2020-05-15 13:05:08 +0800

    added patientlist

---
+++ patientlist.blade.php
@@ -0,0 +1,50 @@
+<!-- patient list table -->
+
+<div class="container-fluid border-top-0 border-bottom-0 rounded shadow-sm">
+			<div class="container">
+				<div class="content-header">
+            
+					<div class="title-primary"><h3 class="text-center"><i class="fa fa-calendar">Patient List</i></h3></div>
+					</div>
+				<div class="row">
+					<table class="table table-hover table-bordered table">
+						<thead class="table-info">
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">First</th>
+								<th scope="col">Last</th>
+								<th scope="col">Handle</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th scope="row">1</th>
+								<td>Mark</td>
+								<td>Otto</td>
+								<td>@mdo</td>
+							</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>Jacob</td>
+								<td>Thornton</td>
+								<td>@fat</td>
+							</tr>
+							<tr>
+								<th scope="row">3</th>
+								<td>Larry the Bird</td>
+								<td>@twitter</td>
+								<td>@twitter</td>
+							</tr>
+							<tr>
+								<th scope="row">4</th>
+								<td>Ju Ne</td>
+								<td>@twitter</td>
+								<td>@twitter</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+
+			
+		</div>
