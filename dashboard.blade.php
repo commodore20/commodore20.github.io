54626e71d44be660d5d21d50a0ba19f65fe98a8e
commodore20 <43285184+commodore20@users.noreply.github.com>
2020-05-18 11:09:41 +0800

    Dashboard
    added dashboard

---
+++ dashboard.blade.php
@@ -0,0 +1,156 @@
+<div class="container-fluid dashbods fant py-3">
+			<div class="container border rounded shadow-sm px-5">
+				<div class="content-header">
+					<div class="title-primary">
+						<h3 class="text-center">
+							<i class="fa fa-calendar-times-o"> </i>
+							Recent Appointments
+						</h3>
+					</div>
+					<div class="row">
+						<div class="table-responsive-md col-md-12">
+							<table class="table table-hover table-bordered table">
+								<thead class="table-info">
+									<tr>
+										<th scope="col">#</th>
+										<th scope="col">Clinic Name</th>
+										<th scope="col">Name of the Doctor</th>
+										<th scope="col">Remarks</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<th scope="row">1</th>
+										<td>St. Jude Clinic</td>
+										<td>Otto Sotto</td>
+										<td>Follow up checkup after 3 days</td>
+									</tr>
+									<tr>
+										<th scope="row">2</th>
+										<td>Bansalan Clinic</td>
+										<td>Jon Bovi</td>
+										<td>Finished</td>
+									</tr>
+									<tr>
+										<th scope="row">3</th>
+										<td>St. Therese Clinic</td>
+										<td>Ma. Jessa Conception</td>
+										<td>Cancelled</td>
+									</tr>
+									<tr>
+										<th scope="row">4</th>
+										<td>Xiao Xiu Clinic</td>
+										<td>Tang San</td>
+										<td>Finished</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div
+						class="col-md-12 border rounded shadow-sm py-3 table-responsive-md"
+					>
+						<h3 class="text-center">
+							<i class="fa fa-exclamation-circle"> </i>
+							Notifications
+						</h3>
+
+						<div class="alert alert-info alert-dismissable">
+							<a class="panel-close close" data-dismiss="alert">×</a> Your
+							appointment to Doctor Kuack has been <strong>approved</strong>.
+						</div>
+						<table class="table table-hover table-striped">
+							<tbody>
+								<tr>
+									<td>
+										<span class="float-right font-weight-bold">3 hrs ago</span>
+										Here is your a link to the latest summary report from the..
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<span class="float-right font-weight-bold">Yesterday</span>
+										There has been a request on your account since that was..
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<span class="float-right font-weight-bold">9/10</span>
+										You have successfully finished your appointment last
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<span class="float-right font-weight-bold">9/4</span>
+										You have cancelled your appointment, please enter a valid
+										reason
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<span class="float-right font-weight-bold">9/4</span>
+										Dr. Medina wants you to have a follow up check up this May
+										26
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="row">
+					<div
+						class="col-md-12 border rounded shadow-sm pt-3 table-responsive-md"
+					>
+						<h3 class="text-center">
+							<i class="fa fa-bookmark"> </i>
+							Your Bookings
+						</h3>
+
+						<div class="alert alert-info alert-dismissable">
+							<a class="panel-close close" data-dismiss="alert">×</a> You still
+							have to come back for the follow-up check up by
+							<strong>Dr. Black Jack</strong>.
+						</div>
+						<table class="table table-hover table-bordered table">
+							<thead class="table-info">
+								<tr>
+									<th scope="col">#</th>
+									<th scope="col">Name of Doctor</th>
+									<th scope="col">Date</th>
+									<th scope="col">Time of Appointment</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>Black Jack</td>
+									<td>June 20, 2019</td>
+									<td>8:00 - 9:00 am</td>
+								</tr>
+								<tr>
+									<th scope="row">2</th>
+									<td>Jacob Leighs</td>
+									<td>January 1, 2020</td>
+									<td>1:00 - 2:00 pm</td>
+								</tr>
+								<tr>
+									<th scope="row">3</th>
+									<td>Larry the Bird</td>
+									<td>May 25, 2020</td>
+									<td>6:00 - 7:00 pm</td>
+								</tr>
+								<tr>
+									<th scope="row">4</th>
+									<td>Ju Ne Maruku</td>
+									<td>May 4, 2020</td>
+									<td>3:00 - 5:00 pm</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
